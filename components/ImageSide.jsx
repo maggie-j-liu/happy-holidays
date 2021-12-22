4feb9e7a44a510e8f638bccbc351b533c329875f
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-21 23:19:36 -0800

    penguin

--- components/ImageSide.jsx
+++ components/ImageSide.jsx
@@ -1,5 +1,13 @@
+import penguin from "../public/penguin.gif";
+import Image from "next/image";
 const ImageSide = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-1/2">
+        <Image src={penguin} />
+      </div>
+    </div>
+  );
 };
 
 export default ImageSide;
