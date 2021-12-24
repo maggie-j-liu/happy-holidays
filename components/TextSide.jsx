66814383e154920e20ed9dea44cabdbc88ff52bd
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-24 14:06:00 -0800

    don't limit message width

--- components/TextSide.jsx
+++ components/TextSide.jsx
@@ -1,6 +1,6 @@
 const TextSide = ({ message }) => {
   return (
-    <div className="whitespace-pre-wrap p-2 sm:p-4 text-xs sm:text-base w-full h-full prose">
+    <div className="max-w-none whitespace-pre-wrap p-2 sm:p-4 text-xs sm:text-base w-full h-full prose">
       {message}
     </div>
   );
